c860102f3ca4d2c0d5a370d1d1dba2eea49322b9
Juan <juan.p.gomsa@gmail.com>
2022-01-11 16:17:21 +0100

    Release v1.0.8

---
+++ src/OrganisationService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace HiCo\EventManagerService;
+
+use GuzzleHttp\Client as GuzzleClient;
+use HiCo\EventManagerClient\Service\OrganisationApi;
+
+
+class OrganisationService
+{
+    private Client $client;
+
+    public function __construct(GuzzleClient $guzzleClient)
+    {
+        $this->client = new Client($guzzleClient);
+    }
+
+    public function createOrganisation(string $organisationId): void
+    {
+        $apiInstance = new OrganisationApi($this->client::getClient(), $this->client::getConfig());
+
+        $apiInstance->createOrganisationIndex($organisationId);
+    }
+
+}
